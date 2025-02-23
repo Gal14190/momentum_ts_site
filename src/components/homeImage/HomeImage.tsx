7a9a8598529da949c9acb9d4d807c64acb88f99a
Yehuda Briskman <yr0556772363@gmail.com>
2025-02-23 20:46:57 +0200

    create first commit and first deploy

---
+++ src/components/homeImage/HomeImage.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import classes from './homeImage.module.css'
+import hebrew from '../../assets/images/momentumHe.png'
+import english from '../../assets/images/momentumEn.png'
+
+const HomeImage = () => {
+    const [lang, setLang] = useState<"he" | "en">("en")
+
+    return (
+        <div className={classes.container}>
+            <img
+                src={lang === "he" ? hebrew : english}
+                alt={lang === "he" ? "Hebrew Website" : "English Website"}
+                className={classes.image}
+            />
+            <button className={classes.switchButton} onClick={() => setLang(lang === "he" ? "en" : "he")}>
+                {lang === "he" ? "🇺🇸 English" : "🇮🇱 עברית"}
+            </button>
+        </div>
+    )
+}
+
+export default HomeImage
---
+++ src/pages/homepage/HomePage.tsx
@@ -0,0 +1,12 @@
+import HomeImage from '../../components/homeImage/HomeImage';
+// import classes from './homePage.module.css'
+
+function HomePage() {
+  return (
+    // <div className={classes.container}>
+      <HomeImage/>
+    // </div>
+  );
+}
+
+export default HomePage;

aec6196e52f816f4e1d11345726a3f9c9a0553bc
Yehuda Briskman <yr0556772363@gmail.com>
2025-03-20 20:53:24 +0200

    update main layout

--- src/layouts/mainLayOut/MainLayOut.tsx
+++ src/layouts/mainLayOut/MainLayOut.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import classes from "./mainLayOut.module.css";
+import MainLayOutNavBar from "./mainLayOutNavBar/MainLayOutNavBar";
 
 const MainLayOut: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  return <div className={classes.mainContainer}>{children}</div>;
+  return <>
+    <MainLayOutNavBar />
+    <div className={classes.mainContainer}>{children}</div>;
+  </>
 };
 
 export default MainLayOut;

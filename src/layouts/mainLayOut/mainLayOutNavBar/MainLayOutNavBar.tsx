e7c973794aa38b4bff6fe443d71778f5ec82be2b
Yehuda Briskman <yr0556772363@gmail.com>
2025-03-25 13:10:15 +0200

    update location of navbar button and update user management of user data from token

--- src/layouts/mainLayOut/mainLayOutNavBar/MainLayOutNavBar.tsx
+++ src/layouts/mainLayOut/mainLayOutNavBar/MainLayOutNavBar.tsx
@@ -1,17 +1,26 @@
 import { useLocation } from "react-router-dom";
 import classes from "./mainLayOutNavBar.module.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getTokenData } from "../../../store/storeUtils";
 import { UserData } from "../../../types/api/users";
 
 const MainLayOutNavBar = () => {
      const location = useLocation();
-     const userData: UserData = getTokenData<UserData>("user") || {
-          role: "user",
-          username: "user",
-     };
+     const defaultUser: UserData = { username: "Guest", role: "user" };
+     const userData: UserData = getTokenData<UserData>("user") ?? defaultUser
      const [isOpen, setIsOpen] = useState(false);
+
+     useEffect(() => {
+          const handleResize = () => {
+               if (window.innerWidth > 768 && isOpen) {
+                    setIsOpen(false);
+               }
+          };
+          window.addEventListener("resize", handleResize);
+          handleResize();
+          return () => window.removeEventListener("resize", handleResize);
+     }, [isOpen]);
 
      const links = [
           { path: "/", label: "Home" },
@@ -23,7 +32,7 @@ const MainLayOutNavBar = () => {
           <>
                <div className={`${classes.curved1} ${isOpen ? classes.navbarOpen : ""}`} />
                <div className={`${classes.curved2} ${isOpen ? classes.navbarOpen : ""}`} />
-               <button className={`${classes.menuButton} ${isOpen ? classes.menuButtonOpen : ""}`} onClick={() => setIsOpen(!isOpen)}>
+               <button aria-label="Toggle Navigation Menu" className={`${classes.menuButton} ${isOpen ? classes.menuButtonOpen : ""}`} onClick={() => setIsOpen(!isOpen)}>
                     <div className={`${classes.line} ${isOpen ? classes.lineTop : ""}`}></div>
                     <div className={`${classes.line} ${isOpen ? classes.lineMiddle : ""}`}></div>
                     <div className={`${classes.line} ${isOpen ? classes.lineBottom : ""}`}></div>

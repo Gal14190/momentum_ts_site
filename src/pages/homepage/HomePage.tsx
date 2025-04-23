66ddddd0786bd1a8accabb9212982692fe4dcd5f
Yehuda Briskman <yr0556772363@gmail.com>
2025-04-23 17:09:01 +0300

    update homepage

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,13 +1,29 @@
-import HomeImage from '../../components/homeImage/HomeImage';
+// import ContactUs from '../../components/home/contact/ContactUs';
+import Footer from '../../components/home/footer/Footer';
+import HomeInfo from '../../components/home/homeInfo/HomeInfo';
+import HomeInfo2 from '../../components/home/homeInfo2/HomeInfo2';
+import WhyMomentum from '../../components/home/whyMomentum/WhyMomentum';
 import classes from './homePage.module.css'
 
 function HomePage() {
   return (
-    <div className={classes.container}>
-      <div className={classes.container_item}>
-        <HomeImage />
+    <>
+      <div className={classes.container}>
+        <div className={classes.container_item}>
+          <HomeInfo />
+        </div>
+        <div className={classes.container_item}>
+          <HomeInfo2 />
+        </div>
+        <div className={classes.container_item}>
+          <WhyMomentum />
+        </div>
+        {/* <div className={classes.container_item}>
+          <ContactUs />
+        </div> */}
       </div>
-    </div>
+      {/* <Footer /> */}
+    </>
   );
 }
 

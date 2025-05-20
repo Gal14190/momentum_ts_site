e61da998d14566e085b0497ffe8439638e57a6ac
Yehuda Briskman <yr0556772363@gmail.com>
2025-05-20 11:04:32 +0300

    update pictures

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -4,20 +4,35 @@ import HomeInfo from '../../components/home/homeInfo/HomeInfo';
 import HomeInfo2 from '../../components/home/homeInfo2/HomeInfo2';
 import WhyMomentum from '../../components/home/whyMomentum/WhyMomentum';
 import classes from './homePage.module.css'
+import { motion } from 'framer-motion';
 
 function HomePage() {
   return (
     <>
       <div className={classes.container}>
-        <div className={classes.container_item}>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className={classes.container_item}
+        >
           <HomeInfo />
-        </div>
-        <div className={classes.container_item}>
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.5 }}
+          className={classes.container_item}
+        >
           <HomeInfo2 />
-        </div>
-        <div className={classes.container_item}>
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.5 }}
+          className={classes.container_item}
+        >
           <WhyMomentum />
-        </div>
+        </motion.div>
         {/* <div className={classes.container_item}>
           <ContactUs />
         </div> */}
